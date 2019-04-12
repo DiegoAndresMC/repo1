e218e65743a96ca6ec5b7ee2bc5aaf8ce605e931
DiegoAndresMC <diego.andresmc@gmail.com>
2019-04-11 22:57:49 -0400

    otro2

---
+++ url-redirect/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hola"
+
+@app.route("/start")
+def start():
+    return url_for("start", next="login")
+
+@app.route("/google")
+def go_to_google():
+    return redirect("https://www.google.com")
+
+@app.route("/post/<int:id>")
+def post(id):
+    return "El Post: {}".format(id)
+
+@app.route("/today")
+def today():
+    return redirect(url_for("post", id=50, next="edit"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
