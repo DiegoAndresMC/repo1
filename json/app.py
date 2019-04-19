8f56f2faae57a88c65468057b88f2cd5293e8bcd
DiegoAndresMC <diego.andresmc@gmail.com>
2019-04-19 18:15:25 -0400

    Agregando json

--- json/app.py
+++ json/app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-app = Flask (__name__)
+from flask import Flask, jsonify
 
-@app . route ( '/' )
-def index ():
+app = Flask(__name__)
+
+cns_videos = [
+    {
+        "id": 1,
+        "title": "1.- Hola - Curso Flask"
+    },
+    {
+        "id": 2,
+        "title": "2.- metodo fun - Curso Flask"
+    }
+        ]
+
+isc_videos = [
+    {
+        "id": 1,
+        "title": "1.- Hola - Curso Flask"
+    },
+    {
+        "id": 2,
+        "title": "2.- metodo fun - Curso Flask"
+    }
+        ]
+
+
+@app.route('/')
+def index():
     return 'Hola'
 
+@app.route("/api/v1/videos/")
+def get_all_videos():
+    return jsonify({"videos": {"cns": cns_videos, "isc": isc_videos}})
+
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
